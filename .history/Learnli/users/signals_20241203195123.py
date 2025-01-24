c8a2db9a62b4578dd62d5235f25bc30155d5177c
Mubiru23 <87474988+Mubiru23@users.noreply.github.com>
2025-01-24 09:10:50 +0300

    Delete .history directory

--- .history/Learnli/users/signals_20241203195123.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import FlaggedContent
-
-@receiver(post_save, sender=FlaggedContent)
-def mark_content_as_reported(sender, instance, created, **kwargs):
-    if created:
-        instance.content_object.reported = True
-        instance.content_object.save()
